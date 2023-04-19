e1324d9a8c08d4601a1cd386fa8e73e5f8a06070
Hamza <rekikhamza11@gmail.com>
2023-04-19 02:32:40 +0100

    Updated messages

--- ArtyProd/ArtyProd/views.py
+++ ArtyProd/ArtyProd/views.py
@@ -1,43 +1,45 @@
-from django.shortcuts import render , redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib import messages
 
+
 def main(request):
-    return render(request,'main.html')
+    return render(request, "main.html")
+
 
 def login_view(request):
-    if request.method == 'POST':
-        username = request.POST['email']
-        password = request.POST['password']
+    if request.method == "POST":
+        username = request.POST["email"]
+        password = request.POST["password"]
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            messages.success(request, 'You have been logged in.')
-            return redirect('main') # replace 'home' with the URL name of your home page
+            return redirect("main")
         else:
-            messages.error(request, 'Invalid login credentials.')
-    return render(request,'login.html')
+            messages.error(request, "Invalid username or password.")
+    return render(request, "login.html")
+
 
 def signin_view(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         # Retrieve user data from POST request
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        password = request.POST.get('password')
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
         # Create new user
-        user = User.objects.create_user(username=username, email=email, password=password)
-        messages.success(request, 'Your account has been created.')
-        return redirect('main') # replace 'home' with the URL name of your home page
-    else:
-        return render(request,'signin.html')
+        user = User.objects.create_user(
+            username=username, email=email, password=password
+        )
+        messages.success(request, "Your account has been created.")
+
+    return render(request, "signin.html")
 
 
 def contact(request):
-    return render(request,'contact.html')
-
+    return render(request, "contact.html")
 
 
 def logout_view(request):
     logout(request)
-    return redirect('main')
+    return redirect("main")
