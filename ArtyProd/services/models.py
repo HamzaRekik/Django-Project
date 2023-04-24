7af1b0a41b4a2f23673d10d239211d48b440b2a9
HamzaRekik <rekikhamza11@gmail.com>
2023-04-24 18:52:35 +0200

    Updated Contact Model.

--- ArtyProd/services/models.py
+++ ArtyProd/services/models.py
@@ -14,3 +14,10 @@ class Service(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Contact (models.Model):
+    name = models.CharField( max_length=50)
+    email = models.EmailField( max_length=254)
+    phone = models.CharField(max_length=8)
+    message = models.TextField()
